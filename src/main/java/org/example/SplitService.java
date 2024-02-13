ea6b4061e316a9194a385c3555455acd1d69d331
Arachneee <jhg2819@naver.com>
2024-02-13 18:01:25 +0900

    refactor: 메소드 분리

--- src/main/java/org/example/SplitService.java
+++ src/main/java/org/example/SplitService.java
@@ -1,72 +1,73 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class SplitService {
 
-    public static final Pattern NUMBERIC_PATTERN = Pattern.compile("^[0-9]+$");
 
     public static final String PREFIX_SEPARATOR = "//";
     private static final String SUFFIX_SEPARATOR = "\n";
+    public static final Pattern CUSTOM_PATTERN = Pattern.compile("^" + PREFIX_SEPARATOR + "(.*)" + SUFFIX_SEPARATOR + "(.*)");
+    private static final String DEFAULT_SEPARATOR = ",|;";
 
     public static int sum(String text) {
-        if (text == null || text.isBlank()) {
+        if (text == null) {
             return 0;
         }
 
-        String[] arr = split(text);
+        final String separator = findSeparator(text);
+        final String source = findSource(text);
+        final List<String> stringNumbers = split(source, separator);
 
-        if (arr.length == 0) {
-            return 0;
-        }
-
-        return Arrays.stream(arr)
+        return stringNumbers.stream()
                 .mapToInt(SplitService::convertInt)
                 .sum();
     }
 
-    public static String[] split(String text) {
-        boolean condition = isCustom(text);
-        if (condition) {
+    private static String findSeparator(String text) {
+        if (isCustom(text)) {
             int preIndex = text.indexOf(PREFIX_SEPARATOR);
             int sufIndex = text.indexOf(SUFFIX_SEPARATOR);
 
-            String substring = findSubstring(text);
-            if (substring.isBlank()) {
-                return new String[]{"0"};
-            }
-
-            return substring.split(text.substring(preIndex + 2, sufIndex));
-        } else {
-            return text.split(",|:");
+            return text.substring(preIndex + 2, sufIndex);
         }
+
+        return DEFAULT_SEPARATOR;
+    }
+
+    private static String findSource(String text) {
+        if (isCustom((text))) {
+            int index = text.indexOf(SUFFIX_SEPARATOR);
+            return text.substring(index + 1);
+        }
+
+        return text;
     }
 
     public static boolean isCustom(String value) {
-        if (value.contains("//") && value.contains("\n")) {
-            int preIndex = value.indexOf(PREFIX_SEPARATOR);
-            int sufIndex = value.indexOf(SUFFIX_SEPARATOR);
-            if (preIndex < sufIndex) {
-                return true;
-            }
-            throw new IllegalArgumentException("입력이 잘못되었습니다.");
-        }
-        return false;
+        final Matcher matcher = CUSTOM_PATTERN.matcher(value);
+        return matcher.find();
     }
 
-    public static String findSubstring(String text) {
-        int index = text.indexOf(SUFFIX_SEPARATOR);
-        return text.substring(index + 1);
+    public static List<String> split(String text, String separator) {
+        return Arrays.stream(text.split(separator))
+                .collect(Collectors.toList());
     }
 
     private static int convertInt(String value) {
         try {
+            if (value == null || value.isBlank()) {
+                return 0;
+            }
+
             int number = Integer.parseInt(value);
             if (number < 0) {
                 throw new RuntimeException();
             }
-
             return number;
         } catch (NumberFormatException e) {
             throw new RuntimeException(e);
--- src/test/java/org/example/SplitServiceTest.java
+++ src/test/java/org/example/SplitServiceTest.java
@@ -1,10 +1,15 @@
 package org.example;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.in;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -12,51 +17,55 @@ class SplitServiceTest {
 
     @DisplayName("커스텀 구분자 추출")
     @Test
-    void test() {
+    void isCustom() {
         // given
         // when
         boolean result = SplitService.isCustom("//a\n1a2a3");
 
         // then
-        Assertions.assertThat(result).isTrue();
+        assertThat(result).isTrue();
     }
 
-    @DisplayName("")
-    @Test
-    void sum() {
+    @DisplayName("더하기")
+    @ParameterizedTest
+    @CsvSource({"'//a\n1a2a3',6", "'//ab\n2ab5ab7',14", "'1,2,3;4',10", "'//\n1234',10"})
+    void sum(String input, int expect) {
         // given
-
         // when
-        int result = SplitService.sum("//a\n1a2a3");
+        int result = SplitService.sum(input);
 
         // then
-        Assertions.assertThat(result).isEqualTo(6);
+        assertThat(result).isEqualTo(expect);
     }
 
+    @DisplayName("빈문자는 0이 나온다.")
     @ParameterizedTest
     @ValueSource(strings = {"", "//a\n"})
     @NullSource
-    void emtry(String value) {
+    void empty(String value) {
         // given
+        // when
         int sum = SplitService.sum(value);
 
-        // when
-
-
         // then
-        Assertions.assertThat(sum).isEqualTo(0);
+        assertThat(sum).isEqualTo(0);
     }
 
-    @DisplayName("")
+    @DisplayName("음수는 예외가 발생한다.")
     @Test
     void negative() {
         // given
-        Assertions.assertThatThrownBy(() -> SplitService.sum("-1")).isInstanceOf(RuntimeException.class);
-
         // when
-
-
         // then
+        assertThatThrownBy(() -> SplitService.sum("-1")).isInstanceOf(RuntimeException.class);
+    }
 
+    @DisplayName("구분자로 분할되지 않으면 예외가 발생한다.")
+    @Test
+    void test() {
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> SplitService.sum("//a\n1a2,3")).isInstanceOf(RuntimeException.class);
     }
 }
